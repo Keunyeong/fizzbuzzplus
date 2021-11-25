b9f2cbad74773019e93156c5911b2391431cbf34
keunyeong <91kyeong@gmail.com>
2021-11-25 13:50:35 +0900

    fix: 'pizzzzz'

--- fizzbuzzplus.js
+++ fizzbuzzplus.js
@@ -2,7 +2,7 @@ for(let i=1; i<=30; i++){
   if(i%15===0){
     console.log('fizzbuzz');
   }else if(i%3===0){
-    console.log('fizz');
+    console.log('pizzzzzzz');
   }else if(i%5===0){
     console.log('buzz');
   }else{
